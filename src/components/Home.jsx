b145daa681f336a42c12c175b5aa05937bf15746
John Robling <roblingjohn@gmail.com>
2020-09-09 14:50:19 -0400

    added basic information to home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className='mainBox'>
+      <h1>
+        We’ve made the difficult decision to postpone (again) until September
+        25th, 2021.
+      </h1>
+      <p>
+        In all instances, the health and safety of our family, friends, vendors,
+        and cats is our number one priority. We want to extend a giant thank you
+        to all of you for understanding. Please don’t hesitate to reach out to
+        us if you need anything to help move your reservations. In our
+        experience, airlines and hotels are being extremely accommodating during
+        this difficult time.{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Home;
