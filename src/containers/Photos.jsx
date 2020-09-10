be2d3fdfbd70264dfba3fbfa0bf1426a1766fc22
John Robling <roblingjohn@gmail.com>
2020-09-10 17:29:10 -0400

    added bootstrap navbar, created routes to pages

---
+++ src/containers/Photos.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Photos = () => {
+    return (
+        <div>
+            <h1>This is the photos page.</h1>
+        </div>
+    );
+};
+
+export default Photos;
---
+++ src/containers/WeddingParty.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WeddingParty = () => {
+    return (
+        <div>
+            <h1>This is the wedding party page.</h1>
+        </div>
+    );
+};
+
+export default WeddingParty;
