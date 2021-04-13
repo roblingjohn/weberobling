16273d62db45df4f9795187adcfc98085355679a
John Robling <roblingjohn@gmail.com>
2021-04-13 12:47:50 -0400

    made sections conditional

--- src/containers/RSVP.jsx
+++ src/containers/RSVP.jsx
@@ -42,69 +42,82 @@ class RSVP extends Component {
               <label for="no">No</label>
             </div>
           </div>
-          <div className="row">
-            <div className="col col-sm-3">Will you be bringing a guest? </div>
-            <div className="col col-sm-2">
-              <input type="radio" id="yes" name="canAttend" value="yes" />
-              <label for="yes">Yes</label>
-              <input type="radio" id="no" name="canAttend" value="no" />
-              <label for="no">No</label>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col col-sm-3">
-              <label for="name">Guest name</label>{" "}
-            </div>
-            <div className="col col-sm-2">
-              <input type="text" id="name" name="name" value=""></input>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col col-sm-3">Number of children:</div>
-            <div className="col col-sm-2">
-              <select name="chidren" id="children">
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col col-sm-3">
-              <label for="mainFood">Food Selection: </label>{" "}
-            </div>
-            <div className="col col-sm-2">
-              <select name="mainFood" id="mainFood">
-                <option value="Buffalo Gal">Buffalo Gal</option>
-                <option value="Other Selection">Other Selection</option>
-              </select>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col col-sm-3">
-              <label for="mainFood">Food Selection: </label>{" "}
-            </div>
-            <div className="col col-sm-2">
-              <select name="mainFood" id="mainFood">
-                <option value="Buffalo Gal">Buffalo Gal</option>
-                <option value="Other Selection">Other Selection</option>
-              </select>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col col-sm-3">
-              <label for="songRequest">Any song request?</label>{" "}
-            </div>
-            <div className="col col-sm-2">
-              <input
-                type="text"
-                id="songRequest"
-                name="songRequest"
-                value=""
-              ></input>
-            </div>
-          </div>
+          {this.state.canAttend && (
+            <>
+              <div className="row">
+                <div className="col col-sm-3">
+                  Will you be bringing a guest?{" "}
+                </div>
+                <div className="col col-sm-2">
+                  <input type="radio" id="yes" name="canAttend" value="yes" />
+                  <label for="yes">Yes</label>
+                  <input type="radio" id="no" name="canAttend" value="no" />
+                  <label for="no">No</label>
+                </div>
+              </div>
+              {this.state.hasGuest && (
+                <>
+                  <div className="row">
+                    <div className="col col-sm-3">
+                      <label for="name">Guest name</label>{" "}
+                    </div>
+                    <div className="col col-sm-2">
+                      <input type="text" id="name" name="name" value=""></input>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col col-sm-3">Number of children:</div>
+                    <div className="col col-sm-2">
+                      <select name="chidren" id="children">
+                        <option value="0">0</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                      </select>
+                    </div>
+                  </div>
+                </>
+              )}
+
+              <div className="row">
+                <div className="col col-sm-3">
+                  <label for="mainFood">Food Selection: </label>{" "}
+                </div>
+                <div className="col col-sm-2">
+                  <select name="mainFood" id="mainFood">
+                    <option value="Buffalo Gal">Buffalo Gal</option>
+                    <option value="Other Selection">Other Selection</option>
+                  </select>
+                </div>
+              </div>
+              {this.state.hasGuest && <><div className="row">
+                <div className="col col-sm-3">
+                  <label for="mainFood">Food Selection: </label>{" "}
+                </div>
+                <div className="col col-sm-2">
+                  <select name="mainFood" id="mainFood">
+                    <option value="Buffalo Gal">Buffalo Gal</option>
+                    <option value="Other Selection">Other Selection</option>
+                  </select>
+                </div>
+              </div> </>}
+              
+              <div className="row">
+                <div className="col col-sm-3">
+                  <label for="songRequest">Any song request?</label>{" "}
+                </div>
+                <div className="col col-sm-2">
+                  <input
+                    type="text"
+                    id="songRequest"
+                    name="songRequest"
+                    value=""
+                  ></input>
+                </div>
+              </div>
+            </>
+          )}
+
           <button>Submit</button>
         </form>
       </div>
