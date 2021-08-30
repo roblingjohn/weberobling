fa64d6bf9cb819664cf54b7750fdbb8076014d6f
John Robling <roblingjohn@gmail.com>
2021-08-30 00:40:47 -0400

    did that fix it?

---
+++ src/containers/RehearsalDinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RehearsalDinner = () => {
+    return (
+        <div>
+            <h1>Rehearsal Dinner</h1>
+            <p>Information coming soon!</p>
+        </div>
+    );
+};
+
+export default RehearsalDinner;
